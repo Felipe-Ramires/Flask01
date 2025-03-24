746708f5f42fcf9b2774bb2770f7f97ce9172db9
Felipe Ramires <felipe.ramires482@gmail.com>
2025-03-23 21:45:29 -0300

    23/03

--- routes/clientes.py
+++ routes/clientes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from database.cliente import CLIENTES
 
 cliente_route = Blueprint('cliente', __name__)
@@ -9,7 +9,18 @@ def lista_clientes():
 
 @cliente_route.route('/', methods=['POST'])
 def inserir_cliente():
-    pass
+    data = request.json
+
+    novo_usuario = {
+        "id": len(CLIENTES)+1,
+        "nome": data['nome'],
+        "email": data['email'],
+
+    }
+
+    CLIENTES.append(novo_usuario)
+
+    return render_template('item_cliente.html', cliente=novo_usuario)
 
 @cliente_route.route('/new')
 def form_cliente():
